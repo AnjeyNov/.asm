7a332c0a79e64079286411129b7b9066fd4c887c
AnjeyNov <AnjeyNov@gmail.com>
2019-02-21 19:52:27 +0300

    add output

--- KPIYAP_LAB_3.asm
+++ KPIYAP_LAB_3.asm
@@ -157,29 +157,51 @@ sum_loop2:
    jo    sum_error
    inc   SI
    inc   SI  
-   loop  sum_loop2
-   
-   
+   loop  sum_loop2   
 after_sum_error:   
    pop   CX
    inc   DI
    inc   DI
    loop  sum_loop1
+   jmp   OutputResult
    
 sum_error:                  ;error of sum
    mov  result[DI], 00h
    jmp  after_sum_error 
    
 
-;;;;;;loop for calculate the sum in a row;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;loop for calculate the sum in a row;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
 ;;;;;;END;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-        
+                                                                                                                                  
+                                                                                                                                  
+OutputResult: 
+
+   mov  AH, 09h
+   lea  DX, message2
+   int 21h
+   
+   xor  CX, CX
+   mov  CL, rows
+   xor  SI, SI
+output_loop:
+   mov  AX, result[SI]
+   call OutInt
+   inc  SI
+   inc  SI
+   
+   mov  AH, 02h
+   mov  DL, ' '
+   int 21h
+   
+   loop output_loop
+   jmp exit
+                                                                                                                                     
            
 ;;;;;;process to get the number;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;START;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
            
-get_number proc
-    
+get_number proc          ; void get_number (void)
+                         ; {
    push  CX                    ; push CX, BX, SI, DI to stack
    push  BX 
    push  SI 
@@ -234,53 +256,51 @@ converting:                  ; form string to int
    mov   DX, 1               ; DX = 1 - discharge multiplier
    push  DX                  ; push DX to stack
    xor   AX, AX              ; AX = 0 - value of discharge                                
-converting_loop:
-   mov   AL, number[SI]      ; AL = number[SI]
-   cmp   AL, '-'             ; ? AL == '-' goto to_neg
+converting_loop:             ; do{
+   mov   AL, number[SI]      ;      AL = number[SI]
+   cmp   AL, '-'             ;      ? AL == '-' goto to_neg
    je    to_neg              ;
-   and   AX, 0Fh             ; AL -= 0Fh (48) - char to int 
-   mul   DX                  ; AX = AX*DX, DX=0
-   pop   DX                  ; pop DX from stack
-   jo    overflow
-   add   BX, AX
-   js    overflow
-   jo    overflow
-   cmp   SI, 02h
-   je    end_get
-   xchg  AX, DX
-   mov   DX, 10
-   mul   DX
-   jo    overflow
-   xchg  DX, AX
-   push  DX
-   dec   SI
-   loop  converting_loop
-   jmp   end_get
+   and   AX, 0Fh             ;      AL -= 0Fh (48) - char to int 
+   mul   DX                  ;      AX = AX*DX, DX=0
+   pop   DX                  ;      pop DX from stack
+   jo    overflow            ;      ? OF==1 goto overflow (overflow flag)
+   add   BX, AX              ;      BX +=AX
+   js    overflow            ;      ? SF==1 goto overflow (sign flag)
+   jo    overflow            ;      ? OF==1 goto overflow (overflow flag)
+   cmp   SI, 02h             ;      ? SI==02h goto end_get
+   je    end_get             ;      02h - start of number
+   xchg  AX, DX              ;      AX <-> DX
+   mov   DX, 10              ;      DX = 10
+   mul   DX                  ;      AX = AX*DX
+   jo    overflow            ;      ? OF==1 goto overflow (overflow flag)
+   xchg  DX, AX              ;      AX <-> DX
+   push  DX                  ;      push DX to stack
+   dec   SI                  ;      --SI
+   loop  converting_loop     ; } while(CX==0)
+   jmp   end_get             ; goto end_get
    
    
-to_neg:
-   pop   DX
-   neg   BX
-   jo    overflow
-   jmp   end_get
+to_neg:                      ; convert to neg
+   pop   DX                  ; pop form stack 
+   neg   BX                  ; BX = -BX conversion to reverse code
+   jo    overflow            ; ? OF==1 goto overflow (overflow flag)
+   jmp   end_get             ; goto end_get
 
 
-overflow:
+overflow:                    ; overflow
    mov   AH, 09h
    lea   DX, message6
    int   21h
-   jmp   start_gc 
-
-end_get:
-   
-   mov   AX, BX
-   ;pop DX? 
-   pop   DI 
-   pop   SI 
-   pop   BX 
-   pop   CX
-   ret     
-get_number endp        
+   jmp   start_gc            ; enter again
+                             
+end_get:                   
+   mov   AX, BX              ; result to AX
+   pop   DI                  ; recovery DI, SI, BX. CX
+   pop   SI                  ;
+   pop   BX                  ;
+   pop   CX                  ;
+   ret                       ; return
+get_number endp          ; }  
    
 
 ;;;;;;process to get the number;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -323,14 +343,5 @@ OutInt endp
        
    
 exit:  
-   mov AX, [SI]
-   call OutInt
-   mov DX, ' '
-   int 21h
-   inc SI
-   inc SI
-   dec CX
-   cmp CX, 0
-   jne exit
        
 end START
